f06c1fdb88d343fb41ee41306cdad9269aa14e4e
Julien Lafont <jla@zenexity.com>
2014-06-27 16:52:30 +0200

    fix score

--- static/js/outputs/score.js
+++ static/js/outputs/score.js
@@ -2,12 +2,12 @@ var ScoreOut = function() {
 
 }
 
-ScoreOut.prototype.update = function(data, action, events, step) {
+ScoreOut.prototype.update = function(data, action, events, step, teamPlaying) {
   var eventsType = _.map(events, function(event) { return event.type }).join(',')
   var teamPlaying = _.map(events, function(event) { return event.teamPlaying }).join(',')
   if (eventsType === 'GOAL') {
-    if(teamPlaying === '0'){
-      $('#scoreTeam0').text(parseInt( $('#scoreTeam0').html()) + 1);
+    if(teamPlaying == 0){
+      $('#scoreTeam0').text(parseInt( $('#scoreTeam0').text()) + 1);
           $('#image').show();
 
           var hide = function(){
@@ -16,8 +16,8 @@ ScoreOut.prototype.update = function(data, action, events, step) {
           setTimeout(hide, 1500);  // 2 seconds
     }
 
-    if(teamPlaying === '1'){
-      $('#scoreTeam1').text(parseInt( $('#scoreTeam1').html()) + 1);
+    else {
+      $('#scoreTeam1').text(parseInt( $('#scoreTeam1').text()) + 1);
         $('#image').show();
 
         var hide = function(){
