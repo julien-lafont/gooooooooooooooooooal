82ca70d6efbf509ce7881501c80509a24f07d3a3
Maxime Calmels <mca@zenexity.com>
2014-06-27 16:25:09 +0200

    show image on goal

--- static/js/outputs/score.js
+++ static/js/outputs/score.js
@@ -8,10 +8,22 @@ ScoreOut.prototype.update = function(data, action, events, step) {
   if (eventsType === 'GOAL') {
     if(teamPlaying === '0'){
       $('#scoreTeam0').text(parseInt( $('#scoreTeam1').html()) + 1);
+          $('#image').show();
+
+          var hide = function(){
+            $('#image').hide();
+          }
+          setTimeout(hide, 500);  // 2 seconds
     }
 
     if(teamPlaying === '1'){
       $('#scoreTeam1').text(parseInt( $('#scoreTeam1').html()) + 1);
+        $('#image').show();
+
+        var hide = function(){
+          $('#image').hide();
+        }
+        setTimeout(hide, 500);  // 2 seconds
     }
   }
 }
