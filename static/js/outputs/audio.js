09a94de47182c4530eb8ec992a30c0eb7395179d
Maxime Calmels <mca@zenexity.com>
2014-06-27 15:16:09 +0200

    Add goal sound

--- static/js/outputs/audio.js
+++ static/js/outputs/audio.js
@@ -3,11 +3,18 @@ var AudioOut = function() {
 }
 
 AudioOut.prototype.update = function(data, action, events, step) {
-  if ('speechSynthesis' in window && action.type === 'SHOOT') {
-    var goalMsg = new SpeechSynthesisUtterance();
-      goalMsg.text="Et c'est le tir";
-      goalMsg.voice = "Hysterical";
-      goalMsg.lang = 'fr-FR';
-      window.speechSynthesis.speak(goalMsg);
+
+  var eventsType = _.map(events, function(event) { return event.type }).join(',')
+  if (//'speechSynthesis' in window &&
+    eventsType === 'GOAL') {
+    var audio = new Audio('../sound/goal.mp3');
+    audio.play();
+  //   var goalMsg = new SpeechSynthesisUtterance();
+  //     goalMsg.text="Et c'est le but";
+  //     goalMsg.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == "Hysterical"; })[0];
+  //     goalMsg.lang = 'fr-FR';
+  //     goalMsg.range = parseFloat("1");;
+  //     goalMsg.pitch = parseFloat("1");;
+  //     window.speechSynthesis.speak(goalMsg);
   }
 }
